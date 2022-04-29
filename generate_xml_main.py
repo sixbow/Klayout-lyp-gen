0e79c4fc3bb1d92338588edcb35ebc4d8fe5e26e
Sietse de Boer <sietsedeboer22@hotmail.com>
2022-04-29 19:23:32 +0200

    first working version. Now needs implementation

--- generate_xml_main.py
+++ generate_xml_main.py
@@ -4,15 +4,43 @@ doc = Document()
 
 root = doc.createElement('layer-properties')
 doc.appendChild(root)
-layer1 = doc.createElement('properties')
-root.appendChild(layer1)
 
-sublayer1 = doc.createElement('name')
-layer1.appendChild(sublayer1)
+layer_index_arr = ['0','1','2','3','4']
+name_arr = ['NbTiN_GND','SiC','NbTiN_Top','Aluminium']
+color_arr = ['#ffa500','#4b0082','#ff8c00','#0000ff']
+
+for i in range(3):
+    #Begin: This block makes the name property for a layer.
+    layers = doc.createElement('properties')
+    root.appendChild(layers)
+    sublayers = doc.createElement('name')
+    layers.appendChild(sublayers)
+    text = doc.createTextNode(name_arr[i])
+    sublayers.appendChild(text)
+    sublayers = doc.createElement('frame-color')
+    layers.appendChild(sublayers)
+    text = doc.createTextNode(color_arr[i])
+    sublayers.appendChild(text)
+    sublayers = doc.createElement('fill-color')
+    layers.appendChild(sublayers)
+    text = doc.createTextNode(color_arr[i])
+    sublayers.appendChild(text)
+    sublayers = doc.createElement('source')
+    layers.appendChild(sublayers)
+    text = doc.createTextNode(layer_index_arr[i]+'/0@1')
+    sublayers.appendChild(text)
+    #End: of property ("Layer") block.
 
 
-text = doc.createTextNode('Some text here')
-sublayer1.appendChild(text)
+
+
+
+
+
+# End of the document contains empty name 
+placehold_name = doc.createElement('name')
+root.appendChild(placehold_name)
+
 
 xml_str = doc.toprettyxml(indent ="\t") 
   
