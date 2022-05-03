3fc12a142bc0b1e5149817fc886b074795d1a988
Sietse de Boer <sietsedeboer22@hotmail.com>
2022-05-03 12:05:58 +0200

    Made it into a fuction

--- Klayout-lyp-gen.py
+++ Klayout-lyp-gen.py
@@ -1,5 +1,5 @@
 
-from xml.dom.minidom import Document
+
 
 ## Import Ordered dict : This is similar to the type of dict used in PyClewin from sebastiaan.
 import numpy as np
@@ -15,79 +15,85 @@ layers['Polyimide'] = '0ff0f000'
 layers['Aluminum'] = '0fff0000'
 layers['text'] = '05000000'
 ## /Import Ordered dict
+from xml.dom.minidom import Document
 
-print( layers.keys()[1])
-print(layers.values()[1][0])
-print(layers.values()[1][1])
-print(layers.values()[1][2:8])
-print(layers['text'])
 
-dict_length = len(layers.keys()) # Obtains the length of the user defined dict. 
-print(dict_length)
-layer_index_arr = list(range(0,dict_length))
-print(layer_index_arr)
-fill_lookup = OrderedDict()
-fill_lookup['f'] = 'I0'
-fill_lookup['3'] = 'I9'
-fill_lookup['2'] = 'I5'
-fill_lookup['5'] = 'I4' # Double hatch is converted to tight hatching in Klayout because Klayout does not have this option.
 
-out_lookup = OrderedDict()
-out_lookup['0'] = 'I0'
-out_lookup['1'] = 'I6'
-out_lookup['2'] = 'I4'
-out_lookup['3'] = 'I5'
-out_lookup['4'] = 'I3'
+def Klayout_lyp_gen(layers, file_name ):
+    print( layers.keys()[1])
+    print(layers.values()[1][0])
+    print(layers.values()[1][1])
+    print(layers.values()[1][2:8])
+    print(layers['text'])
 
-print(layers.values()[0][2:8])
+    dict_length = len(layers.keys()) # Obtains the length of the user defined dict. 
+    print(dict_length)
+    layer_index_arr = list(range(0,dict_length))
+    print(layer_index_arr)
+    fill_lookup = OrderedDict()
+    fill_lookup['f'] = 'I0'
+    fill_lookup['3'] = 'I9'
+    fill_lookup['2'] = 'I5'
+    fill_lookup['5'] = 'I4' # Double hatch is converted to tight hatching in Klayout because Klayout does not have this option.
 
-#try:
-doc = Document()
+    out_lookup = OrderedDict()
+    out_lookup['0'] = 'I0'
+    out_lookup['1'] = 'I6'
+    out_lookup['2'] = 'I4'
+    out_lookup['3'] = 'I5'
+    out_lookup['4'] = 'I3'
 
-root = doc.createElement('layer-properties')
-doc.appendChild(root)
-#layer_index_arr = ['0','1','2','4','5','8','9']
-#name_arr = ['NbTiN_GND','SiC','NbTiN_Top','Aluminium','text','E-beam_Alu','2inch Circle']
-#color_arr = ['#ffa500','#A908B5','#ff4500','#0000ff','#gggg00','#44gg00','#0044gg']
-#pattern_arr = ['I0','I9','I0','I0','I4','I5','I1']
-for i in range(dict_length):
-    #Begin: This block makes the name property for a layer.
-    KL_layers = doc.createElement('properties')
-    root.appendChild(KL_layers)
-    subKL_layers = doc.createElement('name')
-    KL_layers.appendChild(subKL_layers)
-    text = doc.createTextNode(layers.keys()[i])
-    subKL_layers.appendChild(text)
-    subKL_layers = doc.createElement('frame-color')
-    KL_layers.appendChild(subKL_layers)
-    text = doc.createTextNode('#'+layers.values()[i][2:8][::-1])
-    subKL_layers.appendChild(text)
-    subKL_layers = doc.createElement('fill-color')
-    KL_layers.appendChild(subKL_layers)
-    text = doc.createTextNode('#'+layers.values()[i][2:8][::-1])
-    subKL_layers.appendChild(text)
-    subKL_layers = doc.createElement('dither-pattern')
-    KL_layers.appendChild(subKL_layers)
-    text = doc.createTextNode(fill_lookup[layers.values()[i][1]])
-    subKL_layers.appendChild(text)
-    subKL_layers = doc.createElement('line-style')
-    KL_layers.appendChild(subKL_layers)
-    text = doc.createTextNode(out_lookup[layers.values()[i][0]])
-    subKL_layers.appendChild(text)
-    subKL_layers = doc.createElement('source')
-    KL_layers.appendChild(subKL_layers)
-    text = doc.createTextNode(str(layer_index_arr[i])+'/0@1')
-    subKL_layers.appendChild(text)
-    #End: of property ("Layer") block.
+    print(layers.values()[0][2:8])
 
-# End of the document contains empty name 
-placehold_name = doc.createElement('name')
-root.appendChild(placehold_name)
-xml_str = doc.toprettyxml(indent ="\t") 
+    try:
+        doc = Document()
+
+        root = doc.createElement('layer-properties')
+        doc.appendChild(root)
+        #layer_index_arr = ['0','1','2','4','5','8','9']
+        #name_arr = ['NbTiN_GND','SiC','NbTiN_Top','Aluminium','text','E-beam_Alu','2inch Circle']
+        #color_arr = ['#ffa500','#A908B5','#ff4500','#0000ff','#gggg00','#44gg00','#0044gg']
+        #pattern_arr = ['I0','I9','I0','I0','I4','I5','I1']
+        for i in range(dict_length):
+            #Begin: This block makes the name property for a layer.
+            KL_layers = doc.createElement('properties')
+            root.appendChild(KL_layers)
+            subKL_layers = doc.createElement('name')
+            KL_layers.appendChild(subKL_layers)
+            text = doc.createTextNode(layers.keys()[i])
+            subKL_layers.appendChild(text)
+            subKL_layers = doc.createElement('frame-color')
+            KL_layers.appendChild(subKL_layers)
+            text = doc.createTextNode('#'+layers.values()[i][2:8][::-1])
+            subKL_layers.appendChild(text)
+            subKL_layers = doc.createElement('fill-color')
+            KL_layers.appendChild(subKL_layers)
+            text = doc.createTextNode('#'+layers.values()[i][2:8][::-1])
+            subKL_layers.appendChild(text)
+            subKL_layers = doc.createElement('dither-pattern')
+            KL_layers.appendChild(subKL_layers)
+            text = doc.createTextNode(fill_lookup[layers.values()[i][1]])
+            subKL_layers.appendChild(text)
+            subKL_layers = doc.createElement('line-style')
+            KL_layers.appendChild(subKL_layers)
+            text = doc.createTextNode(out_lookup[layers.values()[i][0]])
+            subKL_layers.appendChild(text)
+            subKL_layers = doc.createElement('source')
+            KL_layers.appendChild(subKL_layers)
+            text = doc.createTextNode(str(layer_index_arr[i])+'/0@1')
+            subKL_layers.appendChild(text)
+            #End: of property ("Layer") block.
+
+        # End of the document contains empty name 
+        placehold_name = doc.createElement('name')
+        root.appendChild(placehold_name)
+        xml_str = doc.toprettyxml(indent ="\t") 
+        
+
+        with open(save_path_file, "w") as f:
+            f.write(xml_str) 
+
+    except:
+        print("Klayout-lyp-gen-addon: Whoops-a-daisy. These are not supported parameters for Klayout-lyp-gen-addon!. Not generating lyp file")
 save_path_file = "KLayout_Layer_properties.lyp" #Name of the xml file
-
-with open(save_path_file, "w") as f:
-    f.write(xml_str) 
-
-#except:
- #   print("Klayout-lyp-gen-addon: Whoops-a-daisy. These are not supported parameters for Klayout-lyp-gen-addon!. Not generating lyp file")
+Klayout_lyp_gen(layers,save_path_file)
